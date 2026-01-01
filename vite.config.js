79f835a72c7ffcf7bdd986e56a25e9c9080fd3bf
edgar.treischl <edgar.treischl@isb.bayern.de>
2026-01-01 16:43:17 +0100

    base arg

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/vuetemplate/',  // <--- essential for GitHub Pages
+  // For GitHub Pages deployment
+  base: process.env.VITE_BASE_URL || '/vuetemplate/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
